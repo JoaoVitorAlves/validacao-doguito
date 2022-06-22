d8125abe30fd3f548e29ef1191a06bca0aa6d27f
João Vitor Alves de Oliveira <jv.joaovitor.oliveira@gmail.com>
2022-06-22 17:12:45 -0300

    validacao

--- js/validacao.js
+++ js/validacao.js
@@ -1,9 +1,42 @@
 
-function valida(input) {
+export function valida(input) {
     const tipoDeInput = input.dataset.tipo
 
     if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
+    }
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHtml = ''
+    } else {
+        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.querySelector('.input-mensagem-erro').innerHtml = mostraMensagemDeErro(tipoInput, input)
+    }
+
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const mensagemDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode esta vazio.'
+    },
+    email: {
+        valueMissing: 'O campo email não pode esta vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar.'
     }
 }
 
@@ -17,6 +50,18 @@ const validadores = {
 dataNascimento.addEventListener('blur', (evento) => {
     validaDataNascimento(evento.target)
 })
+
+function mostraMensagemDeErro(tipoInput, input) {
+    let mensagem = ''
+
+    tiposDeErro.forEach(erro => {
+        if (input.validity[erro]) {
+            mensagem = mensagemDeErro[tiposDeErro][erro]
+        }
+    })
+
+    return mensagem;
+}
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
